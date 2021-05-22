6f9714bf5d71d467eeae9a6e617c54300717447a
Geronymos <spielerbereich@gmail.com>
2021-05-22 03:06:48 +0200

    Updates

---
+++ precache-manifest.170a6c41e6e0f59126baefab8d8e6710.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72de9f026ad9e2bf80135fa07212d655",
+    "url": "/luise-news/index.html"
+  },
+  {
+    "revision": "f613e1559d220e0f2812",
+    "url": "/luise-news/static/css/4.e7607f2c.chunk.css"
+  },
+  {
+    "revision": "9fd825b228ff448189fd",
+    "url": "/luise-news/static/css/main.d88541d3.chunk.css"
+  },
+  {
+    "revision": "91996621c518a3d90337",
+    "url": "/luise-news/static/js/0.826c0dfe.chunk.js"
+  },
+  {
+    "revision": "3996ba3d301ab78cf626",
+    "url": "/luise-news/static/js/10.5a21a220.chunk.js"
+  },
+  {
+    "revision": "640a4f88149a9cece8f6",
+    "url": "/luise-news/static/js/11.ecb0065c.chunk.js"
+  },
+  {
+    "revision": "d8faea5d3ecbd6276f8e",
+    "url": "/luise-news/static/js/12.4d1ef0e6.chunk.js"
+  },
+  {
+    "revision": "2d6a00c455390275ab9f",
+    "url": "/luise-news/static/js/13.885bfb01.chunk.js"
+  },
+  {
+    "revision": "6ade309825fb3db66676",
+    "url": "/luise-news/static/js/14.db7a9a52.chunk.js"
+  },
+  {
+    "revision": "d559edf033c8ba13a197",
+    "url": "/luise-news/static/js/15.fae714d1.chunk.js"
+  },
+  {
+    "revision": "4c3a2f5a9b6f294a6f16",
+    "url": "/luise-news/static/js/16.03de644a.chunk.js"
+  },
+  {
+    "revision": "42eb53674494674bc1b3",
+    "url": "/luise-news/static/js/17.f7acb209.chunk.js"
+  },
+  {
+    "revision": "fa793a4ff38a2876ee65",
+    "url": "/luise-news/static/js/18.b577c2ae.chunk.js"
+  },
+  {
+    "revision": "1be1cc08ed7765af9c1d",
+    "url": "/luise-news/static/js/19.275199f0.chunk.js"
+  },
+  {
+    "revision": "101abe839c0d619fdc1d",
+    "url": "/luise-news/static/js/20.70e640e3.chunk.js"
+  },
+  {
+    "revision": "bfbb3e5f2a05f5f09159",
+    "url": "/luise-news/static/js/21.4488d1cd.chunk.js"
+  },
+  {
+    "revision": "6f5bd7874b4594bf9971",
+    "url": "/luise-news/static/js/22.1190c3d7.chunk.js"
+  },
+  {
+    "revision": "f1f5b46df12e3945c40d",
+    "url": "/luise-news/static/js/23.6da7ec99.chunk.js"
+  },
+  {
+    "revision": "7bc392ad8f11fea7a728",
+    "url": "/luise-news/static/js/24.58397a46.chunk.js"
+  },
+  {
+    "revision": "b871a8d9da6f4bd89996",
+    "url": "/luise-news/static/js/25.7b8d48f2.chunk.js"
+  },
+  {
+    "revision": "43229f3b11d687b3e09d",
+    "url": "/luise-news/static/js/26.a926dedf.chunk.js"
+  },
+  {
+    "revision": "908dcec34ba1ea05c388",
+    "url": "/luise-news/static/js/27.8929f0be.chunk.js"
+  },
+  {
+    "revision": "3f73ce185c67ca26746d",
+    "url": "/luise-news/static/js/28.04618dfd.chunk.js"
+  },
+  {
+    "revision": "f6d36f0b73167e067695",
+    "url": "/luise-news/static/js/29.8fe32afe.chunk.js"
+  },
+  {
+    "revision": "d544109b963876948c07",
+    "url": "/luise-news/static/js/30.04a45b99.chunk.js"
+  },
+  {
+    "revision": "fa4a8ad42f27faccadf7",
+    "url": "/luise-news/static/js/31.5e900aec.chunk.js"
+  },
+  {
+    "revision": "bbf885a55ee5ea1504d4",
+    "url": "/luise-news/static/js/32.9a9d38a3.chunk.js"
+  },
+  {
+    "revision": "0627814db1d6d45f05f8",
+    "url": "/luise-news/static/js/33.5b3cc85e.chunk.js"
+  },
+  {
+    "revision": "4ed8fd0d073a8b205f3e",
+    "url": "/luise-news/static/js/34.d75af821.chunk.js"
+  },
+  {
+    "revision": "5a9493c9cb9c71f3b811",
+    "url": "/luise-news/static/js/35.5e481b96.chunk.js"
+  },
+  {
+    "revision": "255ce427b29c35ae5542",
+    "url": "/luise-news/static/js/36.8213b277.chunk.js"
+  },
+  {
+    "revision": "591a1294a45b13628ee4",
+    "url": "/luise-news/static/js/37.cf5926e7.chunk.js"
+  },
+  {
+    "revision": "0d7cf3070bb2796f073d",
+    "url": "/luise-news/static/js/38.69b2522b.chunk.js"
+  },
+  {
+    "revision": "570cf0de16f47d7e403b",
+    "url": "/luise-news/static/js/39.a6ace768.chunk.js"
+  },
+  {
+    "revision": "f613e1559d220e0f2812",
+    "url": "/luise-news/static/js/4.fb9b8e82.chunk.js"
+  },
+  {
+    "revision": "7e461177274fe0301f07d5f2afc3e48f",
+    "url": "/luise-news/static/js/4.fb9b8e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1df8eb60050c044707a",
+    "url": "/luise-news/static/js/40.fc3b222a.chunk.js"
+  },
+  {
+    "revision": "3cafe7c6ddcb69129423",
+    "url": "/luise-news/static/js/41.379e05ab.chunk.js"
+  },
+  {
+    "revision": "a2c3d03556a93e910500",
+    "url": "/luise-news/static/js/42.727b4891.chunk.js"
+  },
+  {
+    "revision": "88a7adbffc3db983390d",
+    "url": "/luise-news/static/js/43.1fb70882.chunk.js"
+  },
+  {
+    "revision": "48f8ab0d8bd2b6c3dea7",
+    "url": "/luise-news/static/js/44.f12b56d8.chunk.js"
+  },
+  {
+    "revision": "5638631f019dd9c4de26",
+    "url": "/luise-news/static/js/45.43b29c23.chunk.js"
+  },
+  {
+    "revision": "c8d4521764282ed9c98b",
+    "url": "/luise-news/static/js/46.0bd689b1.chunk.js"
+  },
+  {
+    "revision": "fe0f9d935615ac8f56fd",
+    "url": "/luise-news/static/js/47.edd43868.chunk.js"
+  },
+  {
+    "revision": "346397b4828907dae5e5",
+    "url": "/luise-news/static/js/48.ff4393ee.chunk.js"
+  },
+  {
+    "revision": "84847336d353decd8fb4",
+    "url": "/luise-news/static/js/49.768b1815.chunk.js"
+  },
+  {
+    "revision": "0852ea6534484198f0ef",
+    "url": "/luise-news/static/js/5.1bf30240.chunk.js"
+  },
+  {
+    "revision": "8d697f495cae865761b5",
+    "url": "/luise-news/static/js/50.3390568a.chunk.js"
+  },
+  {
+    "revision": "76f54c2f3499543300b4",
+    "url": "/luise-news/static/js/51.7a42b23c.chunk.js"
+  },
+  {
+    "revision": "114c326a5d4f490229b9",
+    "url": "/luise-news/static/js/52.8757368f.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/luise-news/static/js/52.8757368f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c922647d0018b762323d",
+    "url": "/luise-news/static/js/53.baa1935c.chunk.js"
+  },
+  {
+    "revision": "e4e6266fd4483be53048",
+    "url": "/luise-news/static/js/54.8c19a9ae.chunk.js"
+  },
+  {
+    "revision": "a58deff0eae00824ccf0",
+    "url": "/luise-news/static/js/55.53359132.chunk.js"
+  },
+  {
+    "revision": "cd92f6e0df56b7c76714",
+    "url": "/luise-news/static/js/56.50eb34f8.chunk.js"
+  },
+  {
+    "revision": "85b5938388bd9b9c1f9b",
+    "url": "/luise-news/static/js/57.acdae6f0.chunk.js"
+  },
+  {
+    "revision": "f466925627803d13cd91",
+    "url": "/luise-news/static/js/6.5567329c.chunk.js"
+  },
+  {
+    "revision": "71d16436bebcf078fd05",
+    "url": "/luise-news/static/js/7.ed4f6bac.chunk.js"
+  },
+  {
+    "revision": "35980c5e403062297ba3",
+    "url": "/luise-news/static/js/8.2660e391.chunk.js"
+  },
+  {
+    "revision": "3ef088fa1ae1e8fd0ecb",
+    "url": "/luise-news/static/js/9.0f48f0ea.chunk.js"
+  },
+  {
+    "revision": "9fd825b228ff448189fd",
+    "url": "/luise-news/static/js/main.65d07a3f.chunk.js"
+  },
+  {
+    "revision": "42136000c68234bf9603",
+    "url": "/luise-news/static/js/polyfills-dom.0660d118.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/luise-news/static/js/polyfills-dom.0660d118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696ad1dfeb5867cf330c",
+    "url": "/luise-news/static/js/runtime-main.5283568a.js"
+  }
+]);
---
+++ static/js/53.baa1935c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpLuiRSS=this.webpackJsonpLuiRSS||[]).push([[53],{272:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(20),o=e(16),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.baa1935c.chunk.js.map
